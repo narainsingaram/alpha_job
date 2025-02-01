25f2ab9b1b17a08524a5e8a57e3c06532b8d7296
Narain Singaram <narainsingaram7@gmail.com>
2025-02-01 12:41:10 -0500

    Removed Info Page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { UilChannel } from '@iconscout/react-unicons'
-import { UilInfoCircle } from '@iconscout/react-unicons'
 import { UilPostcard } from '@iconscout/react-unicons'
 import { UilAnalytics } from '@iconscout/react-unicons'
 import { UilSignOutAlt } from '@iconscout/react-unicons'
@@ -65,9 +64,6 @@ const NavBar = () => {
         </button>
         <div className={`w-full md:block md:w-auto ${menuOpen ? 'block' : 'hidden'}`} id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a href="/help" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><UilInfoCircle className='inline mb-1 mr-2'/>Info</a>
-            </li>
             <li>
               <a href="/postings" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"><UilPostcard className='inline mb-1 mr-2'/>Postings</a>
             </li>
